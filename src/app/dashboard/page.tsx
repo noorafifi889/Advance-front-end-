e06b228b5fa55a34062ea491e513c9a0c5a4f8e7
noorafifi889 <noorelafifi889@gmail.com>
2026-03-03 21:45:54 +0200

    test a header and sidbar dashboard and do well

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,8 @@
-"use client"
-
-import React from "react"
-import { useDispatch, useSelector } from "react-redux"
-import HeaderDashboard from "@/src/components/dashboard/HeaderDashboard"
-import { logoutThunk } from "@/src/modules/auth/service/authThunks"
+import React from 'react'
 
 const Dashboard = () => {
-  const dispatch = useDispatch()
-
-  const auth = useSelector((state: any) => state.auth.user) //  
-
   return (
-    <div>
-      <HeaderDashboard
-        title="User Dashboard"
-        user={{
-          fullName: auth?.fullName,
-          email: auth?.email,
-          role: auth?.role,
-        }}
-        onLogout={() => dispatch(logoutThunk() as any)}
-        onSearch={(q) => console.log(q)}
-      />
-    </div>
+    <div>Dashboard</div>
   )
 }
 
