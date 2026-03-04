962cad148977dbf68d28c2a022b98f5431d8f7b7
noorafifi889 <noorelafifi889@gmail.com>
2026-03-04 19:20:30 +0200

    add a route to profile dashboard and call it

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,8 @@
+import ProfileDashboard from '@/src/components/dashboard/ProfileDashboard'
+import React from 'react'
+
+const ProfilePage = () => {
+  return (<ProfileDashboard />)
+}
+
+export default ProfilePage
